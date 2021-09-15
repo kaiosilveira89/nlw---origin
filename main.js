a5bb548818c52cf72c743c99cfc1ca568dd77bf1
Kaio <kaiosilveira89@gmail.com>
2021-09-15 16:27:13 -0300

    Tela about

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+/* abre e fecha o menu quando clikar no menu*/
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
+  })
+}
+
+/*quando clickar em item do menu, esconda o mesmo*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
